08c1de7d0d0a0c919ee3381e2cd912e30fd954e0
Tomasz Chybziński <tomasz.chybzinski@gmail.com>
2023-11-20 09:28:14 +0100

    chore: remove revalidate

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,6 @@ import { Metadata } from "next";
 import "./globals.css";
 import "./reset.css";
 
-export const revalidate = 3600;
-
 export const metadata: Metadata = {
   title: "Home",
   description: "Home page, product list page",
